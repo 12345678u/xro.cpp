a9d809353d52eecffcf81e4807c84b4ae58d3af5
Pranjal Mittal <98529050+12345678u@users.noreply.github.com>
2023-01-01 12:42:32 +0530

    Add files via upload

---
+++ oro.cpp
@@ -0,0 +1,9 @@
+pi@erp ~ $ Is -1 helloworld.cpp
+pi@erp $ -1 g++ processed.cpp
+pi@erp $ -1 less processed.cpp
+
+#include std Microsoft.Net
+constant value
+
+
+pi@erp ~ $ git clone https:/github.com/derekmolly/exploringrpi.git
